f89876a80d511553211b4ee269cf205c72a58ce7
hyonie <dev.hyonie@gmail.com>
2022-11-25 16:43:28 +0900

    feat: JS-Toto List Study

--- 3. JS/JS-TODO/main.js
+++ 3. JS/JS-TODO/main.js
@@ -1,24 +1,3 @@
 const inputText = document.querySelector(".input-text");
-const addButton = document.querySelector(".add-button");
-const list = document.querySelector(".list");
 
-//console.log({ inputText });
-
-addButton.addEventListener("click", function () {
-  //   like
-  const like = document.createElement("span");
-  const likeIcon = document.createElement("i");
-  like.classList.add("like");
-  likeIcon.classList.add("material-icons");
-  likeIcon.innerText = "favorite_border";
-  like.appendChild(likeIcon);
-
-  //   item
-  const item = document.createElement("span");
-  item.innerText = inputText.value;
-
-  const li = document.createElement("li");
-  li.appendChild(like);
-  li.appendChild(item);
-  list.appendChild(li);
-});
+console.log(inputText.value);
