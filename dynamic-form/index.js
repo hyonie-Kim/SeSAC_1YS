9c1d7f3d3d1571420ef359632ba0778e62a0a9a2
hyonie <dev.hyonie@gmail.com>
2022-11-25 18:17:58 +0900

    feat: Express Study

--- dynamic-form/index.js
+++ dynamic-form/index.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs"); // 화면 engine을 ejs 설정
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.render("index");
-})
+app.get("/", function (req, res) {
+  res.render("index");
+});
+app.get("/manual", function (req, res) {
+  res.render("manual");
+});
 
-app.get("/form", function(req, res){
-    console.log(req.query);
-    res.send({msg:"get - 이름은:" + req.query.name});
-})
+app.get("/form", function (req, res) {
+  console.log(req.query);
+  res.send({ msg: "get - 이름은:" + req.query.name });
+});
 
-app.post("/form", function(req, res){
-    console.log(req.body);
-    res.send({msg:"post - 이름은:" + req.body.name});
-})
+app.post("/form", function (req, res) {
+  console.log(req.body);
+  res.send({ msg: "post - 이름은:" + req.body.name });
+});
 
-app.listen(port, ()=>{
-    console.log("Server Port:", port);
-})
+app.listen(port, () => {
+  console.log("Server Port:", port);
+});
