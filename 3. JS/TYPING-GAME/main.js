1323270cfd03ea2dbf062145a2ef803ac4eb939d
hyonie <dev.hyonie@gmail.com>
2022-12-05 14:57:29 +0900

    feat: Typing-Game update

--- 3. JS/TYPING-GAME/main.js
+++ 3. JS/TYPING-GAME/main.js
@@ -6,25 +6,77 @@ const messageDisplay = document.querySelector("#message");
 
 const GAME_TIME = 5;
 
-let word = ["HelloWord", "JavaScript", "TypeScript", "React", "Programing"];
+const API_URL = "https://random-word-api.herokuapp.com/word?number=100";
+
+let words = ["HelloWord", "JavaScript", "TypeScript", "React", "Programing"];
 let score = 0;
 let time = 0;
+let timeInterval;
+let isPlaying = false;
+let isReady = false;
+
+asyncInit();
+
+function fetchInit() {
+  const res = fetch(API_URL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      //console.log(data);
+      words = data;
+    });
+}
+
+async function asyncInit() {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+  words = data.filter((item) => {
+    return item.length < 7;
+  });
+  isReady = true;
+  console.log(words);
+}
 
 wordInput.addEventListener("input", (e) => {
   const typedText = e.target.value;
   const currentText = currentWord.innerText;
-  if (typedText.toUpperCase() === currentText.toUpperCase()) {
+  if (typedText.toUpperCase() === currentText.toUpperCase() && isReady) {
     addScore();
     setNewWord();
   }
 });
+// 게임 종료
+function gameOver() {
+  isPlaying = false;
+  clearInterval(timeInterval);
+  timeInterval = null;
+  messageDisplay.innerText = "GAME OVER!";
+  score = 0;
+}
+
+// 시간 카운트 다운
+function countDown() {
+  //console.log(time);
+  time = time - 1;
+  timeDisplay.innerText = time;
+  if (time === 0) {
+    gameOver();
+  }
+}
 
 function setNewWord() {
   time = GAME_TIME;
   wordInput.value = "";
   messageDisplay.innerText = "Now Playing 😃";
-  const randomIndex = Math.floor(Math.random() * word.length);
-  currentWord.innerText = word[randomIndex];
+  const randomIndex = Math.floor(Math.random() * words.length);
+  currentWord.innerText = words[randomIndex];
+
+  if (!isPlaying) {
+    // !isPlaying이 false이면
+    timeInterval = setInterval(countDown, 1000);
+    isPlaying = true;
+  }
 }
 
 function addScore() {
