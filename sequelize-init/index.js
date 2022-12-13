a86d403cc96d134679be70d6020d33448d58f532
hyonie <dev.hyonie@gmail.com>
2022-12-13 13:34:51 +0900

    feat: env update

--- sequelize-init/index.js
+++ sequelize-init/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+
+console.log(process.env.PORT);
+const dotenv = require('dotenv')
+dotenv.config();
+
+console.log(process.env.PORT)
+console.log(process.env)
+
 app.set("view engine", "ejs");
 
 app.use("/static", express.static(__dirname+"/static"));
@@ -19,6 +27,9 @@ app.use('/user', userRouter);
 app.get('*', (req, res)=>{
     res.send("주소가 존재하지 않습니다. 다시 한 번 확인해주세요.");
 });
+
+
+
 
 app.listen(port, ()=>{
     console.log("server open: ", port);
