8cb13ef3c32dfe8aed9c0f32369597c0c7307e56
hyonie <dev.hyonie@gmail.com>
2022-12-13 18:16:27 +0900

    feat: MileEASY_dev update

---
+++ MileEASY_dev/model/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const config = require("../config/config.json")["development"]; //1. 시퀄라이즈, config 불러오기
+
+const db = {};
+
+const sequelize = new Sequelize( // 새로운 시퀄라이즈 객체 생성 (객체 내부에는 db 연결 정보를 가지고 있음)
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = require("./User")(sequelize, Sequelize);
+
+module.exports = db;
---
+++ MileEASY_dev/model/mile.js
@@ -0,0 +1,27 @@
+const User = (Sequelize, DataTypes) => {
+  return Sequelize.define(
+    "user2",
+    {
+      id: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      pw: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "user2",
+      freezeTableName: true,
+      timestamps: false,
+    }
+  );
+};
+
+module.exports = User;
