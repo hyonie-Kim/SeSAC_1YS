945c80208330c266d9112d68af8665e1fcf5f970
hyonie <dev.hyonie@gmail.com>
2023-01-09 23:45:51 +0900

    feat: React Obj

---
+++ React/practice/src/components/profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function profile() {
+  const friendObjArr = [
+    { name: '지수', age: '20', nickName: '츄' },
+    {
+      name: '지선',
+      age: '19',
+      nickName: '지블리',
+    },
+    {
+      name: '은지',
+      age: '20',
+      nickName: '20',
+    },
+  ];
+  return <div>profile</div>;
+}
