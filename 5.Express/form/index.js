ffbcacd5a96cf1fb4d622654b9109a2f8416002f
hyonie <dev.hyonie@gmail.com>
2023-01-20 14:47:53 +0900

    feat: Form get,post Study

--- 5.Express/form/index.js
+++ 5.Express/form/index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "./public/index.html");
 });
@@ -13,7 +15,13 @@ app.get("/calculator", (req, res) => {
   res.send("계산 결과는 ?" + result);
 });
 
-app.get("*", (req, res) => {
+app.post("/calculator", (req, res) => {
+  console.log(req.body);
+  const result = Number(req.body.num1) + Number(req.body.num2);
+  res.send("계산 결과는 ?" + result);
+});
+
+app.all("*", (req, res) => {
   res.status(404).send("찾을수 없는 페이지 입니다.");
 });
 app.listen(port, () => {
