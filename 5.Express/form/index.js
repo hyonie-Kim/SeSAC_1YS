ca6c76fe8a89490629a21e7cd79755c8e456fd95
hyonie <dev.hyonie@gmail.com>
2023-01-23 22:33:23 +0900

    docs: 파일 정리

--- 5.Express/form/index.js
+++ 5.Express/form/index.js
@@ -1,29 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.use(express.static("public"));
 
+app.set("view engine", "ejs"); // ejs 템플릿 설정
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+// app.use("/views", express.static(__dirname) + "/views");
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "./public/index.html");
+  res.render("index");
 });
-
-app.get("/calculator", (req, res) => {
+app.get("/getForm", (req, res) => {
   console.log(req.query);
-  const result = Number(req.query.num1) + Number(req.query.num2);
-  res.send("계산 결과는 ?" + result);
+  // res.send("get 요청 성공!");
+  // const data = { id: req.query.id, pw: req.query.pw, title: "GET 요청" };
+  // res.render("result", { data });
+  res.render("result", {
+    title: "GET 요청",
+    userInfo: req.query,
+  });
 });
 
-app.post("/calculator", (req, res) => {
+app.post("/postForm", (req, res) => {
   console.log(req.body);
-  const result = Number(req.body.num1) + Number(req.body.num2);
-  res.send("계산 결과는 ?" + result);
+  // const data = { id: req.body.id, pw: req.body.pw, title: "POST 요청" };
+  // res.render("result", { data });
+  res.render("result", {
+    title: "POST 요청",
+    userInfo: req.body,
+  });
 });
 
 app.all("*", (req, res) => {
   res.status(404).send("찾을수 없는 페이지 입니다.");
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
