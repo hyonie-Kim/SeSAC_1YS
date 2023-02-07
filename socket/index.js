ef5c0a17cbb84f2912613cc5558d84d9a73a1722
hyonie <dev.hyonie@gmail.com>
2023-02-07 16:05:57 +0900

    feat: Socket 연습

--- socket/index.js
+++ socket/index.js
@@ -1,3 +1,4 @@
+const { createSocket } = require("dgram");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
@@ -10,7 +11,28 @@ app.get("/", (req, res) => {
 // 클라이언트 소켓이 연결이 되면 콜백함수가 실행된다.
 // socket: 클라이언트 소켓과 연결되고 새로 생성된 소켓
 io.on("connection", (socket) => {
-  console.log("Server Socket Conneted", socket.id);
+  console.log("Server Socket Conneted");
+
+  socket.emit("welcome", { msg: "welcome" });
+
+  socket.on("response", (str) => {
+    console.log(str);
+  });
+
+  //   socket.emit("hello", { msg: "hello : 안녕하세요!" });
+  socket.on("hello", (str) => {
+    console.log(str);
+    socket.emit("helloRes", "Hello : 안녕하세요!");
+  });
+  socket.on("study", (str) => {
+    console.log(str);
+    socket.emit("studyRes", "study: 공부합시다!");
+  });
+
+  socket.on("bye", (str) => {
+    console.log(str);
+    socket.emit("byeRes", "bye : 안녕히가세요!");
+  });
 
   socket.on("disconnect", () => {
     console.log("Server Socket disconnected");
