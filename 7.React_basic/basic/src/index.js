702731fe8673269d079b51048d6ed6dc76a44e07
hyonie <dev.hyonie@gmail.com>
2023-02-12 19:38:00 +0900

    feat: 상태를 객체 단위로 보관하고 업데이트

--- 7.React_basic/basic/src/index.js
+++ 7.React_basic/basic/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppXY from "./AppXY";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppProfile from "./AppProfile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProfile />
+    <AppXY />
   </React.StrictMode>
 );
 
