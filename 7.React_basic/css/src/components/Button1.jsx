27d99a2d7ec73de6a94599368900e79ee72eb93e
hyonie <dev.hyonie@gmail.com>
2023-02-13 23:28:37 +0900

    feat: PostCSS

---
+++ 7.React_basic/css/src/components/Button1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Button1.module.css";
+// import "./Button1.css";
+
+export default function Button1() {
+  return <button className={styles.button}>Button1</button>;
+}
