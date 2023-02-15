9cd6cb80282cad55c185891fe79b56051e4b937f
hyonie <dev.hyonie@gmail.com>
2023-02-16 01:11:41 +0900

    feat: todoList

---
+++ 7.React_basic/todo/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: "123", text: "장보기", status: "active" },
+    { id: "124", text: "공부하기", status: "active" },
+  ]);
+
+  const handleAdd = (todo) => {
+    // console.log(todo);
+    setTodos([...todos, todo]);
+  };
+  return (
+    <section>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
+}
