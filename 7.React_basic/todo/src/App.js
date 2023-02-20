87279b3a91b0dd84a5152411eb386a2af417adc9
hyonie <dev.hyonie@gmail.com>
2023-02-20 17:20:04 +0900

    feat: upate

--- 7.React_basic/todo/src/App.js
+++ 7.React_basic/todo/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
     <div>
+      <Header />
       <TodoList />
     </div>
   );
