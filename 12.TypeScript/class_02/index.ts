2d8062ae911fb0b32753d777571165b668bcae79
hyonie <dev.hyonie@gmail.com>
2023-05-07 01:16:32 +0900

    feat: Typescript Dict Class

--- 12.TypeScript/class_02/index.ts
+++ 12.TypeScript/class_02/index.ts
@@ -1,31 +1,50 @@
 type Words = {
-  [key: string]: string;
-};
+  [key:string]:string
+}
 
 // let dict:Words ={ "pizza":"food"}
 
 class Dict {
-  private words: Words;
-  constructor() {
-    this.words = {};
+ private words: Words
+ constructor(){
+  this.words ={}
+ }
+  add(word:Word){
+      if(this.words[word.term] === undefined){
+          this.words[word.term] = word.definition
+      }
   }
-  add(word: Word) {
-    if (this.words[word.term] === undefined) {
-      this.words[word.term] = word.definition;
-    }
+  get(term:string){
+      return this.words[term]
   }
-  get(term: string) {
-    return this.words[term];
+  delete(term:string){
+      delete this.words[term]
   }
-  delete() {}
+  
+  update(word:Word){
+      if(this.words[word.term]!==undefined){
+          this.words[word.term] = word.definition;
+      }
+  }
+  showAll(){
+      console.log(Object.keys(this.words))
+      }
+  
+  count(){
+      return Object.keys(this.words).length;
+  }
+  
 }
 
 class Word {
-  constructor(public term: string, public definition: string) {}
+  constructor(
+      public term:string,
+      public definition:string
+  ){}
 }
 
-const kimchi = new Word("kimchi", "한국의음식");
+const kimchi = new Word("kimchi","한국의음식");
 const dict = new Dict();
 
-dict.add(kimchi);
-dict.get("kimchi");
+dict.add(kimchi)
+dict.get("kimchi")
